f704f435962ee620d6859d98b048f400addb2287
mayeut <mayeut@users.noreply.github.com>
2023-10-21 13:49:07 +0200

    chore: add test for `ninja` script

--- tests/test_ninja.py
+++ tests/test_ninja.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 import subprocess
 import sys
+import sysconfig
 
 import pytest
+from importlib_metadata import distribution
 
 import ninja
 
@@ -17,6 +20,16 @@ def _run(program, args):
     assert excinfo.value.code == 0
 
 
+def _get_scripts():
+    dist = distribution("ninja")
+    scripts_paths = [os.path.abspath(sysconfig.get_path("scripts", scheme)) for scheme in sysconfig.get_scheme_names()]
+    scripts = []
+    for file in dist.files:
+        if os.path.abspath(str(file.locate().parent)) in scripts_paths:
+            scripts.append(file.locate().resolve(strict=True))
+    return scripts
+
+
 def test_ninja_module():
     _run("ninja", ["--version"])
 
@@ -24,4 +37,13 @@ def test_ninja_module():
 def test_ninja_package():
     expected_version = "1.11.1.git.kitware.jobserver-1"
     output = subprocess.check_output([sys.executable, "-m", "ninja", "--version"]).decode("ascii")
+    assert output.splitlines()[0] == expected_version
+
+
+def test_ninja_script():
+    expected_version = "1.11.1.git.kitware.jobserver-1"
+    scripts = _get_scripts()
+    assert len(scripts) == 1
+    assert scripts[0].stem == "ninja"
+    output = subprocess.check_output([str(scripts[0]), "--version"]).decode("ascii")
     assert output.splitlines()[0] == expected_version
