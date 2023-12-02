ee0180228fee3aaf7cc572658919d0beeeb2077b
Matthieu Darbois <mayeut@users.noreply.github.com>
2023-12-02 15:59:34 +0100

    chore: remove `versioneer` related code from `setup.py` (#224)
    * chore: remove `versioneer` related code from `setup.py`
    * remove some missed versioneer related code

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
 from distutils.text_file import TextFile
 
 from skbuild import setup
-
-# Add current folder to path
-# This is required to import versioneer in an isolated pip build
-# Prepending allows not to break on a non-isolated build when versioneer
-# is already installed (c.f. https://github.com/scikit-build/cmake-python-distributions/issues/171)
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 with open('README.rst', 'r') as fp:
     readme = fp.read()
