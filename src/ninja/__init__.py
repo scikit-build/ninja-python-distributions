9d374fe8a01ff090cc069df65495b07be6728f6b
Henry Schreiner <henryschreineriii@gmail.com>
2024-05-21 00:45:10 -0400

    fix: better fallback
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/ninja/__init__.py
+++ src/ninja/__init__.py
@@ -15,34 +15,41 @@ def __dir__():
 
 
 def _get_ninja_dir():
-    ninja_exes = {"ninja" + sysconfig.get_config_var("EXE"), "ninja"}
-    for ninja_exe in ninja_exes:
-        path = os.path.join(sysconfig.get_path("scripts"), ninja_exe)
-        if os.path.isfile(path):
-            return os.path.dirname(path)
+    ninja_exe = "ninja" + sysconfig.get_config_var("EXE")
 
+    # Default path
+    path = os.path.join(sysconfig.get_path("scripts"), ninja_exe)
+    if os.path.isfile(path):
+        return os.path.dirname(path)
+
+    # User path
     if sys.version_info >= (3, 10):
         user_scheme = sysconfig.get_preferred_scheme("user")
     elif os.name == "nt":
         user_scheme = "nt_user"
-    elif sys.platform == "darwin" and sys._framework:
+    elif sys.platform.startswith("darwin") and sys._framework:
         user_scheme = "osx_framework_user"
     else:
         user_scheme = "posix_user"
 
-    for ninja_exe in ninja_exes:
-        path = os.path.join(sysconfig.get_path("scripts", scheme=user_scheme), ninja_exe)
+    path = sysconfig.get_path("scripts", scheme=user_scheme)
 
-        if os.path.isfile(path):
-            return os.path.dirname(path)
+    if os.path.isfile(os.path.join(path, ninja_exe)):
+        return path
 
-    return None
+    # Fallback to python location
+    path = os.path.dirname(sys.executable) 
+    if os.path.isfile(os.path.join(path, ninja_exe)):
+        return path
+
+    return ""
+
 
 BIN_DIR = _get_ninja_dir()
 
-
 def _program(name, args):
-    return subprocess.call([os.path.join(BIN_DIR, name)] + args, close_fds=False)
+    cmd = os.path.join(BIN_DIR, name)
+    return subprocess.call([cmd] + args, close_fds=False)
 
 
 def ninja():
